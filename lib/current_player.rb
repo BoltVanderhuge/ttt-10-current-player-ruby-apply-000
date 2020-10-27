3e8d0e6c1780f24d53c4cdb79a981434a394b301
John Wisneski <john.j.wisneski@gmail.com>
2020-10-27 17:45:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,11 @@
 board = ["", " ", " ", " ", "", " ", " ", " ", ""]
 def turn_count(board)
-board.each do |turn|
-
+  board.each do |turn|
+  puts "Turn #{turn}"
+  end
+end
 
 
 
 def current_player(board)
-  
+end
